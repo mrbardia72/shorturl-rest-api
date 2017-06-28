57aea291e4a80693e43e5b2b7d03d68bc65b4c6a
sajjad kazemi <sajad.kazemi1993@gmail.com>
2017-06-28 20:18:28 +0430

    Main file for transaction

---
+++ url.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by: phpstorm
+ * programmer: sajjad kazemi
+ * Email: sajad.kazemi1993@gmail.com
+ * telegram: @sajjadkazemi10
+ * Date: 2017-06-10
+ * location: iran,gilan,langroud
+ */
+
+/* include file(method)*/
+include_once 'includes/config.php';
+include_once 'includes/function.php';
+
+/*get url and key*/
+    if (isset($_GET['url']) && ($_GET['key']))
+    {
+    $path = $_GET['url'];
+    $kay = $_GET['key'];
+
+
+
+    /* call class shorturl */
+    $obj= new shorturl();
+   $obj->auth_user($kay , $path);
+}
